40d660a05c7b5ab90b36e82236374327085c84a3
abcdefghijklmnopqrstuvwxnasim <abcdefghijklmnopqrstuvwxnasim@gmail.com>
2026-05-19 01:08:14 +0600

    Update nasim-direct-checkout.php

--- nasim-direct-checkout.php
+++ nasim-direct-checkout.php
@@ -4,7 +4,7 @@
  * Description: কাস্টম Elementor Buy Button যা প্রোডাক্ট কার্টে অ্যাড করে সরাসরি WooCommerce Checkout-এ নিয়ে যায়।
  * Plugin URI: https://nasimwebpro.com
  * Author: A.S.M Nasim
- * Version: 1.0.0
+ * Version: 1.0.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
